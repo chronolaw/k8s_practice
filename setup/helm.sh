306c53c1588400852bffc6b15d6577ee30cc226d
chronolaw <chrono_cpp@me.com>
2023-11-13 01:29:12 +0000

    gateway.yml
    routes.yml
    
    helm.sh
    
    gateway install
    
    gateway.yml
    
    traffic.yml
    
    filters.yml

--- setup/helm.sh
+++ setup/helm.sh
@@ -3,9 +3,31 @@
 # chrono @ 2023-11
 
 # https://github.com/helm/helm/releases
-wget https://get.helm.sh/helm-v3.13.2-linux-arm64.tar.gz
 
-tar xfz helm-v3.13.2-linux-arm64.tar.gz
+# VER=v3.13.2
+# ARCH=arm64
+# wget https://get.helm.sh/helm-${VER}-linux-${ARCH}.tar.gz
+# tar xfz helm-${VER}-linux-${ARCH}.tar.gz
+# sudo install linux-${ARCH}/helm /usr/local/bin
 
-sudo install linux-arm64/helm /usr/local/bin
+# install helm
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+# install kong gateway
+helm repo add kong https://charts.konghq.com
+helm repo update
+
+helm repo list
+helm search repo kong
+
+#helm install kong kong/ingress -n kong --create-namespace
+
+# enable http queries
+# traditional_compatible or expressions
+helm install \
+    kong kong/ingress \
+    -n kong \
+    --create-namespace \
+    --set gateway.env.router_flavor=expressions
+
 
